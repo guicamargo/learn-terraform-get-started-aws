949de83e287968a9a91d9443cf153663fd28af9e
Jamal <seu-email@exemplo.com>
2026-01-21 21:07:07 -0300

    commit inicial

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-061fe7df6ad657197"
+  instance_type = "t2.micro"
+  key_name = "learn-terraform"
+
+  tags = {
+    Name = "learn-terraform"
+  }
+}
